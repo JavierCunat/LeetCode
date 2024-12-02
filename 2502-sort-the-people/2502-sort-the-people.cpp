0321d35d9722fe24b4518db1fb8f2fde531b93e8
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2024-12-01 21:01:37 -0500

    Time: 19 ms (35.51%), Space: 28.9 MB (20.29%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        map<int, string> map;
+        vector<string> res;
+        int j = 0;
+        for(string name : names) {
+            map[heights[j]] = name;
+            j++;
+        }
+
+        //lamda function same thing as passing comp as third argument
+        sort(heights.begin(), heights.end(), [](int a, int b)  {
+            return a > b;
+        });
+
+        for(int height : heights) {
+            res.push_back(map[height]);
+        }
+
+        return res;
+
+        //looking back can actually use a priority queue
+    }
+};
