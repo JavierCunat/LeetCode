28cd02b2ff8c5efb204d664fcb34c947ac18a833
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2024-12-02 13:12:03 -0500

    Time: 0 ms (100%), Space: 8.1 MB (6.87%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        map<int,int> memo = { {0, 0}, {1,1}, {2,2}, {3,3} }; 
+        return climbStairsHelper(n, memo);
+    }
+
+    int climbStairsHelper(int n, map<int,int>& memo) {
+        if(memo.count(n) > 0) {
+            return memo[n];
+        } else{
+            int result = climbStairsHelper(n-1, memo) + climbStairsHelper(n-2, memo);
+            memo[n] = result;
+            return result;
+        }
+    }
+};
