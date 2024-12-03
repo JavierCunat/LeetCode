3262c24b99cbb084f335f0ec2b0f7e2fe9d791f1
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2024-12-03 14:26:54 -0500

    Time: 66 ms (5.72%), Space: 137.4 MB (5.31%) - LeetHub

---
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        string res = "";
+        vector<string> strings;
+        int j = 0;
+        int index = 0;
+
+        //put all words in a vec based on spaces ints
+        for(int i = 0; i < s.size(); i++) {
+            if(j < spaces.size() && i == spaces[j]) {
+                strings.push_back(s.substr(index, i - index));
+                index = i;
+                j++;
+            }
+        }
+
+        //doesn't get the last word in the vec so brute for this lmao
+        strings.push_back(s.substr(index));
+
+        //construct the result string
+        for(int i = 0; i < strings.size(); i++) {
+            if(i == strings.size()-1) {
+                res.append(strings[i]);
+            } else {
+                res.append(strings[i] + " ");
+            }
+        }
+
+        return res;
+    }
+};
