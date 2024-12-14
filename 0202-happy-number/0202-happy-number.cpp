0d6d2be82ec892848f480164ac2634478fcbb2cd
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2024-12-14 16:55:05 -0500

    Time: 3 ms (19.92%), Space: 8.5 MB (17.64%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> set;
+        
+        while(n!= 1 && !set.count(n)) {
+            set.insert(n);
+            int sum = 0;
+
+            while(n > 0) {
+                int s = n % 10;
+                sum+=s*s;
+                n/=10;
+            }
+
+            n = sum;
+        }
+
+        return n==1;
+    }
+};
