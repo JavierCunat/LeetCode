010c1f9b56a826b23351d49b7c524609bb25f759
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2024-12-22 16:13:25 -0500

    Time: 8 ms (26.71%), Space: 30.1 MB (5.1%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        return validPalindrome(s, 0, s.size()-1, false);
+    }
+
+    bool validPalindrome(string s, int i, int j, bool val) {
+        while(i <= j) {
+            if(s[i] == s[j]) {
+                i++;
+                j--;
+            } else {
+                if(val) {
+                    return false;
+                } else {
+                    return
+                    validPalindrome(s, i + 1, j, true) || 
+                    validPalindrome(s, i, j - 1, true);
+                }   
+            }
+        }
+        return true;
+    }
+
+
+};
