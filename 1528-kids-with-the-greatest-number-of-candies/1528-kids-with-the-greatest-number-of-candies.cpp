07b28b0296f104458a9b70cee1e846f07fd71c16
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2024-12-30 17:13:44 -0500

    Time: 0 ms (100%), Space: 12.5 MB (35.19%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
@@ -4,9 +4,10 @@ public:
          //use max_element method to find the max in the array, then just make sure the i^th elem is bigger than or equal to this when added extraCandies to it
          //thought about sorting to make it faster but I think it would be hard since we have to build an res array with the corresponding positions in its correct place
         int max = *max_element(candies.begin(), candies.end());
-        vector<bool> res(candies.size());
+        int size = candies.size();
+        vector<bool> res(size);
 
-        for(int i = 0; i < candies.size(); i++) {
+        for(int i = 0; i < size; i++) {
             if(candies[i] + extraCandies >= max) {
                 res[i] = true;
             }
