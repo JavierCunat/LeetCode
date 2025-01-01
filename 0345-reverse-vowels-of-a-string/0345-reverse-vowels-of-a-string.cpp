cdd6b6e5b7524874b57a080da5d71eed005fe8cb
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-01-01 07:03:56 -0500

    Time: 1 ms (56.4%), Space: 10.2 MB (57.5%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -11,7 +11,7 @@ public:
     //3. if found swap, if not don't change anything
     //4. return reversed string vowels
 
-    //intuition works but we can optimize from beats 14% to 42%
+    //intuition works but we can optimize from beats 14% to 42% to 100% using precomputed table with all 256 vals
     //use static const for hash set
     //if we find that the first pointer is already not a vowel we can increase and just continue instead of overhead
     //same for second pointer
