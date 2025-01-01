23b12276c54e8809cff102dafa42f87e83ef833e
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-01-01 06:45:23 -0500

    Time: 3 ms (42.72%), Space: 10.5 MB (35.5%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -10,26 +10,29 @@ public:
     //2. two pointer starting from front and back unitl find vowel
     //3. if found swap, if not don't change anything
     //4. return reversed string vowels
+
+    //intuition works but we can optimize
+    //use static const for hash set
+    //if we find that the first pointer is already not a vowel we can increase and just continue instead of overhead
+    //same for second pointer
+    //use swap function instead of making a temp and doing manual swapping
     string reverseVowels(string s) {
     int i = 0;
     int j = s.size()-1;
-    unordered_set<char> vowels = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
+    static const unordered_set<char> vowels = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
 
     while(i <= j) {
-        if(vowels.count(s[i])) {
-            char v1 = s[i];
-            if(vowels.count(s[j])) {
-                char v2 = s[j];
-                s[i] = v2;
-                s[j] = v1;
-                i++;
-                j--;
-            } else {
-                j--;
-            }
-        } else {
+        if(!vowels.count(s[i])) {
             i++;
+            continue;
         }
+        if(!vowels.count(s[j])) {
+            j--;
+            continue;
+        }
+       swap(s[i], s[j]);
+       i++;
+       j--;
     }
     return s;
 }
