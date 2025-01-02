8e2f9feeb1cc30194617cd2c28a0bbfb8ec1d5e7
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-01-02 10:25:44 -0500

    Time: 4 ms (24.11%), Space: 40.3 MB (49.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,11 +5,13 @@ public:
         vector<int> res(size, 1);
         int product = 1;
 
+        //left cumulative product
         for(int i = 0; i < size; i++) {
             res[i] = product;
             product *= nums[i];
         }
 
+        //right cumulative product
         product = 1;
         for(int i = size-1; i >= 0; i--) {
             res[i] *= product;
