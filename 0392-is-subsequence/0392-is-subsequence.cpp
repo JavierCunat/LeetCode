fe046fa3edda561e310ea7daa68b4ae5fbca9631
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-01-05 15:38:03 -0500

    Time: 0 ms (100%), Space: 8.5 MB (47.68%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        //base case
-        if (s.empty()) return true; 
-        if (t.empty()) return false;
+        int i = 0, j = 0; //pointers for each string
+        int sSize = s.size(), tSize = t.size();
 
-        //recursive step
-        if (s[0] == t[0]) {
-            return isSubsequence(s.substr(1), t.substr(1));
-        } else {
-            return isSubsequence(s, t.substr(1));
+        while(i < sSize && j < tSize) {
+            if(s[i] == t[j]) {
+                i++;
+            }
+            j++;
         }
+
+        return i == sSize;
     }
 };
