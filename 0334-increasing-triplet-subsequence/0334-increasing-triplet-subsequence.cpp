cc178b6059dd6b17222323eedd28741e6b7d9bf6
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-01-05 11:01:35 -0500

    Time: 4 ms (27.58%), Space: 115.8 MB (11.69%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
+        //brute force beats 46% in O(n) by storing the min (i), a flag bool and the prev var (j)
+        //changed to use best approach by storing (i) min and (j) mid
         int size = nums.size();
-        bool flag = false;
-        int prev;
-        int min = INT_MAX;
+        int i = INT_MAX, j = INT_MAX;
 
-        for(int i = 0; i < size-1; i++) {
-            if(nums[i] < min) {
-                min = nums[i];
-            }
-
-            if(min < nums[i+1]) {
-                if(flag && prev < nums[i+1]) {
-                    return true;
-                }
-                flag = true;
-                prev = nums[i+1];
+        for(int c = 0; c < size; c++) {
+            if(i >= nums[c]) {
+                i = nums[c];
+            } else if(j >= nums[c]) {
+                j = nums[c];
+            } else {
+                return true;
             }
         }
 
