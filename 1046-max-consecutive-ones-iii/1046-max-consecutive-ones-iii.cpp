b7b25247a3396b90db036b7d0323dba5f03c555b
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-01-10 16:46:38 -0500

    Time: 3 ms (29.8%), Space: 59.3 MB (46.1%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -4,22 +4,19 @@ public:
         int size = nums.size(), j = 0, zeros = 0, maxSeen = 0;
 
         for(int i = 0; i < size; i++) {
-
-           if(nums[i] == 0) {
+            if(nums[i] == 0) {
                 zeros++;
-           }
-
-            //shrink window if zeros exceeds k
-           while(zeros > k) {
-            if(nums[j] == 0) {
-                zeros--;
             }
-            j++;
-           }
+            //shrink window if zeros exceeds k
+            while(zeros > k) {
+                if(nums[j] == 0) {
+                    zeros--;
+                }
+                j++;
+            }
             
             maxSeen = max(maxSeen, i - j + 1);
         }
-
         return maxSeen;
     }
 };
