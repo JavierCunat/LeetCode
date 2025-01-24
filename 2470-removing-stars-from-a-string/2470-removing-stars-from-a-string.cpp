85a64b2b19db2b50f041f8449326d3cd47ebe098
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-01-23 20:34:33 -0800

    Time: 6 ms (96.24%), Space: 29.9 MB (22.78%) - LeetHub

---
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        //personally think two pointers would be easier stack topic threw me off
+        //use reverse to not exceed time limit, when I first tried I did s = s+stack.pop which is n^2
+        string res = "";
+        int size = s.size();
+        stack<char> stack;
+        for(char c : s) {
+            if(!stack.empty() && c == '*') stack.pop();
+            else stack.push(c);
+        }
+
+        while(!stack.empty()) {
+            res+=stack.top();
+            stack.pop();
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
