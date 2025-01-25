d4b28810d48b9a65377bb4fa82f96be7718ce80c
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-01-25 11:01:03 -0800

    Time: 0 ms (100%), Space: 10.7 MB (56.62%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n + 1);
-        for (int i = 0; i <= n; ++i) {
-            ans[i] = ans[i >> 1] + (i & 1);
-        }
-        return ans;
+       vector<int> res(n+1);
+       //dp approach we use the previous elements of the array and right shift and then add 1
+       for(size_t i = 0; i <= n; i++) {
+        res[i] = res[i>>1] + (i & 1);
+       }
+       return res;
     }
 };
