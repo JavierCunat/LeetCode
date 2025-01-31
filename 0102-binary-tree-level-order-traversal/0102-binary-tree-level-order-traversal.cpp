a0804f0ed19ab3012bc954c0d3171ffdb5e3dbf9
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-01-30 21:58:25 -0800

    Time: 0 ms (100%), Space: 16.7 MB (96.88%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,21 +12,38 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
+        //can do both recursively or iteratively with a q
         vector<vector<int>> res;
-        levelOrder(root, 0, res);
+        int level = 0;
+        queue<TreeNode*> q;
+        if(root == nullptr) return res;
+
+        q.push(root);
+
+        while(!q.empty()) {
+            int length = q.size();
+            res.push_back({});
+
+            for(int i = 0; i < length; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                res[level].push_back(node->val);
+                if(node->left != nullptr) q.push(node->left);
+                if(node->right != nullptr) q.push(node->right);
+            }
+            level++;
+        }
         return res;
     }
 
-    void levelOrder(TreeNode* root, int level, vector<vector<int>>& res) {
-        if(root == nullptr) return;
-
-        if(res.size() <= level){
-            res.push_back({});
-        }
-
-        res[level].push_back(root->val);
-
-        levelOrder(root->left, level + 1, res);
-        levelOrder(root->right, level + 1, res);
-    }
+    // void levelOrder(TreeNode* root, int level, vector<vector<int>>& res) {
+    //     if(root == nullptr) return;
+    //     if(res.size() <= level){
+    //         res.push_back({});
+    //     }
+    //     res[level].push_back(root->val);
+    //     levelOrder(root->left, level + 1, res);
+    //     levelOrder(root->right, level + 1, res);
+    // }
 };
