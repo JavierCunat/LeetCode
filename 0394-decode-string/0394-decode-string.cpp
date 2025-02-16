03b7a94d05d11528537391b5af2fac05dcae7d25
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-02-15 20:46:42 -0800

    Time: 0 ms (100%), Space: 9.3 MB (66.9%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> count;
+        stack<string> strings;
+        string currString = "";
+        int currNum = 0;
+
+        for(char c : s) {
+            if(isdigit(c)) {
+                //build the number handles multi-digit cases like 12[abc]
+                currNum = currNum * 10 + (c - '0');
+            } else if(c == '[') {
+                count.push(currNum);
+                strings.push(currString);
+                currNum = 0;
+                currString = "";
+            } else if(c == ']') {
+                int repeatCount = count.top(); count.pop();
+                string temp = strings.top(); strings.pop();
+                while(repeatCount--) temp += currString;
+                currString = temp;
+            } else {
+                currString += c;
+            }
+        }
+
+        return currString;
+    }
+};
