b9792c3486a4ea9b798d49f98fa18dd560bf4ef3
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-02-26 18:27:06 -0800

    Time: 63 ms (57.11%), Space: 99.3 MB (13.33%) - LeetHub

--- 2636-maximum-subsequence-score/2636-maximum-subsequence-score.cpp
+++ 2636-maximum-subsequence-score/2636-maximum-subsequence-score.cpp
@@ -7,15 +7,17 @@ public:
        //since matching pairs I guess thats how we keep the indexes relevant even though we sorted and lose them
        vector<pair<int,int>> pairs;
        for(int i = 0; i < size; i++) {
-        pairs.emplace_back(nums2[i], nums1[i]);
+        pairs.emplace_back(nums2[i], nums1[i]); //like push_back but more efficient with pairs
+        //like pushback({nums2[i], nums1[i]})
        }
-       sort(pairs.rbegin(), pairs.rend());
+       sort(pairs.rbegin(), pairs.rend()); //reverse iterators
 
-      //priority queue to store k largest elements in nums1
-      priority_queue<int, vector<int>, greater<int>> pq;
+      //priority queue to store k largest elements in nums1, greater cmp achieves min-heap behavior
+      priority_queue<int, vector<int>, greater<int>> pq; //even though pqs are always vectors under the hood the third param has to be a cmp function
       long long maxScore = 0, sum = 0;
 
       //iterate through sorted pairs
+      //the min-heap helps us remove the smallest number when the heap exceeds size k
       for(int i = 0; i < size; i++) {
         int minNum2 = pairs[i].first;
         int num1 = pairs[i].second;
@@ -28,6 +30,7 @@ public:
             pq.pop();
         }
 
+        //update max score when we are at a min heap of k elements
         if(pq.size() == k) {
             maxScore = max(maxScore, sum * minNum2);
         }
