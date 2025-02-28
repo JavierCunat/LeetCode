1320410e7b16dab7311a9b84a85931fd4dcc36f7
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-02-28 14:31:03 -0800

    Time: 2 ms (25.45%), Space: 8.7 MB (79.66%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void combinationSum3(int &k, int &n, vector<vector<int>> &res, int currSum, vector<int> &currArr, int index) {
+        if(currArr.size() == k) {
+            if(currSum == n) {
+                res.push_back(currArr);
+            } 
+            return;
+        }
+
+        for(int i = index; i < 10; i++) {
+            currArr.push_back(i); //choose
+            combinationSum3(k, n, res, currSum + i, currArr, i + 1); //explore
+            currArr.pop_back(); //unchoose
+        }
+        
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> res;
+        vector<int> currArr;
+        combinationSum3(k, n, res, 0, currArr, 1);
+        return res;
+    }
+};
