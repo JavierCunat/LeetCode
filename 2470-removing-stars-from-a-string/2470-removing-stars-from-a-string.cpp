8b15d6bcdba14581071a63b44826fe757ca7f1f2
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-03-15 22:21:25 -0700

    Update 2470-removing-stars-from-a-string.cpp

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string removeStars(string s) {
+        //reviewed on 3/15/2025
         //personally think two pointers would be easier stack topic threw me off
         //use reverse to not exceed time limit, when I first tried I did s = s+stack.pop which is n^2
         string res = "";
