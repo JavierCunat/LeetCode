7961e04aa9d4e6679adbf950eb9a3d44071b1fed
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-04-01 20:32:17 -0400

    Time: 59 ms (21.08%), Space: 47.2 MB (20.35%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         //shortest path algo using BFS with queue step one direction each time until find first exit
+        //use in-place marking visited to reduce O(log n) time from using .count() and size/space of the set
         queue<vector<int>> q;
-        set<pair<int,int>> visited;
         int steps = 0;
         int rowSize = maze.size(), colSize = maze[0].size();
         q.push(entrance);
-        visited.insert({entrance[0], entrance[1]});
+        maze[entrance[0]][entrance[1]] = 'v';
 
 
         while(!q.empty()) {
@@ -15,33 +15,33 @@ public:
             while(size--) {
                 vector<int> curr = q.front(); q.pop();
 
-                if(maze[curr[0]][curr[1]] == '.' && 
+                if(
                 curr != entrance &&
                 (curr[0] == 0 || curr[0] == rowSize-1 || curr[1] == 0 || curr[1] == colSize-1) 
                 ) return steps;
 
                 //check up
-                if(curr[0] > 0 && !visited.count({curr[0] - 1, curr[1]}) && maze[curr[0]-1][curr[1]] == '.') {
+                if(curr[0] > 0 && maze[curr[0]-1][curr[1]] == '.') {
                     q.push({curr[0]-1, curr[1]});
-                    visited.insert({curr[0]-1, curr[1]});
+                    maze[curr[0]-1][curr[1]] = 'v';
                 }
                 
                 //check left
-                if(curr[1] > 0 && !visited.count({curr[0], curr[1] - 1})  && maze[curr[0]][curr[1]-1] == '.') {
+                if(curr[1] > 0 && maze[curr[0]][curr[1]-1] == '.') {
                     q.push({curr[0], curr[1]-1});
-                    visited.insert({curr[0], curr[1]-1});
+                    maze[curr[0]][curr[1]-1] = 'v';
                 }
 
                 //check down
-                if(curr[0] < rowSize-1 && !visited.count({curr[0]+1, curr[1]}) && maze[curr[0]+1][curr[1]] == '.' ){
+                if(curr[0] < rowSize-1 && maze[curr[0]+1][curr[1]] == '.' ){
                     q.push({curr[0] + 1, curr[1]});
-                    visited.insert({curr[0] + 1, curr[1]});
+                    maze[curr[0]+1][curr[1]] = 'v';
                 }
 
                 //check right
-                if(curr[1] < colSize-1 && !visited.count({curr[0], curr[1]+1}) && maze[curr[0]][curr[1]+1] == '.' ){
+                if(curr[1] < colSize-1 && maze[curr[0]][curr[1]+1] == '.' ){
                     q.push({curr[0], curr[1] + 1});
-                    visited.insert({curr[0], curr[1] + 1});
+                    maze[curr[0]][curr[1] + 1] = 'v';
                 }
             }
             steps++;
