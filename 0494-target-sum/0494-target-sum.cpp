01d7dbc52860938c286b9750c483839f038c54a6
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-07-28 19:55:58 -0700

    Time: 27 ms (43.32%), Space: 26.4 MB (23.62%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    int backtrack(vector<int>& nums, int target, int index, long currSum) {
-        if(index == nums.size()) {
-            return (target == currSum) ? 1 : 0;
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        vector<vector<int>> dp(nums.size()+1, vector<int>(2 * sum + 1, 0));
+
+        if(abs(target) > sum) return 0;
+
+        //there is 1 way to sum to 0 (sum is 0 since we are going to 2 * sum)
+        dp[0][sum] = 1;
+
+        for(int i = 1; i <= nums.size(); i++) {
+            for(int j = 0; j <= sum*2; j++) {
+                if(j - nums[i-1] >= 0) {
+                    dp[i][j] += dp[i-1][j - nums[i-1]];
+                }
+                if(j + nums[i-1] <= sum * 2) {
+                    dp[i][j] += dp[i-1][j + nums[i-1]];
+                }
+            }
         }
 
-        // add curr number and sub curr number
-        return backtrack(nums, target, index + 1, currSum + nums[index]) + 
-               backtrack(nums, target, index + 1, currSum - nums[index]);
-    }
-
-
-    int findTargetSumWays(vector<int>& nums, int target) {
-        if(nums.empty()) return 0;
-        return backtrack(nums, target, 0, 0);
+        return dp[nums.size()][target + sum];
     }
 };
