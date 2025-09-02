928d12513aff1a430e2ddae1095ab51aee080c67
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-09-02 02:09:23 -0400

    Update 0392-is-subsequence.cpp

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        //reviewed on 09/02/2025
         int i = 0, j = 0; //pointers for each string
         int sSize = s.size(), tSize = t.size();
 
