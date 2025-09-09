fc2bca5ae2d0e2e7a75f70b4af94236482e2cae0
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-09-08 23:12:27 -0700

    Update 0136-single-number.cpp

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        //reviewed 09/08/2025
         size_t size = nums.size();
         int prev = 0;
         for(int i = 0; i < size; i++) {
