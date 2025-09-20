85d40a0cf8e4a917d82697384dc8817b31c44b59
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-09-19 23:23:28 -0700

    Time: 1 ms (29.63%), Space: 35.7 MB (54.69%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-       //reviewed on 8/18/2025
-       //optimized to O(n) by keeping a prefix sum of array
-       int size = nums.size();
-       int lhs = 0;
        int sum = accumulate(nums.begin(), nums.end(), 0);
+       int left = 0;
 
-       for(int i = 0; i < size; i++) {
-        //check if leftsum equals rightsum
-        if(lhs == sum-nums[i]-lhs) { return i; }
-        lhs+=nums[i];
+       for(int i = 0; i < nums.size(); i++) {
+        left+=nums[i];
+        if(left == sum) return i;
+        sum-=nums[i];
        }
 
        return -1;
