9765bf769daa1c529e342b94ce8065aa93dce2a8
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-10-19 18:16:51 -0700

    Time: 2177 ms (5%), Space: 59.2 MB (93.79%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,8 +1,42 @@
 class Solution {
 public:
+
     int findKthLargest(vector<int>& nums, int k) {
-        //two lines and faster? is it possible??
-        nth_element(nums.begin(), nums.begin()+k-1, nums.end(), greater<int>());
-        return nums[k-1];
+        int n = nums.size();
+        int target = n - k;
+        return quickSelect(nums, 0, n-1, target);
     }
+
+private:
+    int partition(vector<int>& A, int left, int right, int pivot) {
+        int pivotVal = A[pivot];
+        swap(A[pivot], A[right]);
+        int store = left;
+        for(int i = left; i < right; i++) {
+            if(A[i] < pivotVal) {
+                swap(A[store++], A[i]);
+            }
+        }
+        swap(A[store], A[right]);
+        return store;
+    }
+
+    int quickSelect(vector<int>& A, int left, int right, int k) {
+        if(left == right) {
+            return A[left];
+        }
+
+        int pivot = left + (right - left) / 2;
+
+        int pivotIdx = partition(A, left, right, pivot);
+
+        if(pivotIdx == k) {
+            return A[pivotIdx];
+        } else if(pivotIdx > k) {
+            return quickSelect(A, left, pivotIdx - 1, k);
+        } else {
+            return quickSelect(A, pivotIdx + 1, right, k);
+        }
+    }
+
 };
