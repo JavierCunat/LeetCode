58ca1c1bda9fe75d3c3e16082b2cb9209294dcb0
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-10-22 13:22:31 -0700

    Time: 0 ms (100%), Space: 8.7 MB (44.11%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0 || x == 1) return 1;
+
+        long long N = n;
+
+        if(N < 0) {
+            x = 1.0 / x;
+            N = -N;
+        }
+
+        double base = x;
+        double res = 1.0;
+
+        while(N > 0) {
+            if(N & 1) res*=base;
+            base *= base;
+            N >>= 1;
+        }
+
+        return res;
+    }
+};
