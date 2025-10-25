301f064aa4be5444e897b12390736500edabd9ab
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-10-24 18:19:48 -0700

    Time: 3 ms (88.42%), Space: 15.3 MB (40.44%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+
+public:
+    int calculate(string s) {
+        int len = s.length();
+        if(len == 0) return 0;
+        stack<int> stack;
+        char op = '+';
+        int currNum = 0;
+
+        for(int i = 0; i < len; i++) {
+            char currChar = s[i];
+            if(isdigit(currChar)) {
+                currNum = (currNum * 10) + (currChar - '0');
+            }
+            if(!isdigit(currChar) && !iswspace(currChar) || i == len-1) {
+                if(op == '-') {
+                    stack.push(-currNum);
+                } else if(op == '+') {
+                    stack.push(currNum);
+                } else if(op == '*') {
+                    int prev = stack.top(); stack.pop();
+                    stack.push(prev * currNum);
+                } else if(op == '/') {
+                    int prev = stack.top(); stack.pop();
+                    stack.push(prev / currNum);
+                }
+                op = currChar;
+                currNum = 0;
+            }
+        }
+
+        int res = 0;
+        while(!stack.empty()) {
+            res += stack.top(); stack.pop();
+        }
+        return res;
+    }
+};
