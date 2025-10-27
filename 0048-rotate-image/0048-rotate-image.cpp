9ac0253472384463c8bd465a2a8333d2a2668c95
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-10-26 22:57:39 -0700

    Time: 0 ms (100%), Space: 10.3 MB (35.11%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
+        int r = matrix.size(), c = matrix[0].size();
 
-        //transpose
-        for(int i = 0; i < n ; i++) {
-            for(int j = i + 1; j < n; j++) {
+        for(int i = 0; i < r; i++) {
+            for(int j = i+1; j < c; j++) {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
 
-        //reverse row
-        for(auto& row : matrix) {
-            reverse(row.begin(), row.end());
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c/2; j++) {
+                swap(matrix[i][j], matrix[i][c - j - 1]);
+            }
         }
     }
 };
