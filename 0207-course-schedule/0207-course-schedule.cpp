a44feba7eed46eb20b3e27a60d2b7a503ae468a9
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-10-29 17:19:48 -0700

    Time: 0 ms (100%), Space: 19.1 MB (85.56%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        //check for cycle only way it is impossible to finish the classes is if there's a cycle
+        //khans algo to find topological sorting of graph
+        //put everything into adj list vertex -> other vertex
+        //track the number of indegrees and start at the node with indegree of 0, then delete that edge and repeat
+        int n = prerequisites.size(), m = prerequisites.size();
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegrees(numCourses, 0);
+        int visited = 0;
 
-        //kahns algo
-        //add all nodes with no dependencies (in-degree) to the queue then get rid of the edges
-        //graph will cycle with have (in-dgree) greater than one
-        vector<int> indegree(numCourses, 0); //keep track of indegree per node with index being node
-        vector<vector<int>> adj(numCourses); //directed node to edge
-        for(auto& prereq : prerequisites) {
-            adj[prereq[1]].push_back(prereq[0]);
-            indegree[prereq[0]]++;
+        for(int i = 0; i < n; i++) {
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indegrees[prerequisites[i][0]]++;
         }
 
         queue<int> q;
         for(int i = 0; i < numCourses; i++) {
-            if(indegree[i] == 0) q.push(i);
+            if(indegrees[i] == 0) q.push(i);
         }
+        
 
-        int visited = 0;
         while(!q.empty()) {
             int node = q.front(); q.pop();
             visited++;
+            
             for(auto neighbor : adj[node]) {
-                indegree[neighbor]--; //symbolically removing edge
-                if(indegree[neighbor] == 0) {
-                    q.push(neighbor);
-                }
+                indegrees[neighbor]--;
+                if(indegrees[neighbor] == 0) q.push(neighbor);
             }
+
         }
 
+        return visited == numCourses;
 
-        return numCourses == visited;
+
+
+
+
     }
 };
