bc73db712aa85242f0918c3fcf27b264daab873d
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2025-12-08 00:52:39 -0800

    Time: 5 ms (42.62%), Space: 9.4 MB (50.95%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int n1 = s1.length(), n2 = s2.length(), n3 = s3.length();
+
+        if (n1 + n2 != n3) return false;
+
+        vector<vector<bool>> dp(n1 + 1, vector<bool>(n2 + 1, false));
+        dp[0][0] = true; //both empty strings can form an empty string
+
+        //fill first column only using s1
+        for(int i = 1; i <= n1; i++) {
+            dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
+        }
+
+        //fill first row only using s2
+        for(int j = 1; j <= n2; j++) {
+            dp[0][j] = dp[0][j-1] && (s2[j-1] == s3[j-1]);
+        }
+
+        for (int i = 1; i <= n1; ++i) {
+            for (int j = 1; j <= n2; ++j) {
+                char c = s3[i + j - 1];
+                dp[i][j] = (dp[i-1][j] && s1[i-1] == c) || (dp[i][j-1] && s2[j-1] == c);
+            }
+        }
+       
+
+        return dp[n1][n2];
+    }
+};
