c1c6403c9d80efec9a63ee6443d71516ba399fbb
Javier Cunat <54507881+JavierCunat@users.noreply.github.com>
2026-01-05 23:26:57 -0800

    Add review note in largestAltitude function
    Added a review note for future reference.

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
+        //reviewed 1/5/2026
         int maxSeen = 0, size = gain.size(), curr = 0;
 
         for(int i = 0; i < size; i++) {
